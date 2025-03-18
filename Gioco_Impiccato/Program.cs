7c8ac236856f91bd6d17bc903f8757cd17b6e454
angelicapazzagli <pazzagli.angelica@franchettisalviani.net>
2025-03-18 22:44:24 +0100

    seconda_versione

--- Gioco_Impiccato/Program.cs
+++ Gioco_Impiccato/Program.cs
@@ -1,40 +1,46 @@
 ﻿Random r = new Random();
-string scelta_livello()
+string scelta_livello(ref int n_tentativi, ref string livello)
 {
     bool scelta = false;
     while (scelta == false)
     {
-        Console.WriteLine("Scegli il livello di difficoltà:\n1. Facile\n2. Medio\n3. Difficile");
-        string livello = Console.ReadLine();
+        Console.WriteLine("\nScegli il livello di difficoltà:\n1. Facile\n2. Medio\n3. Difficile");
+        livello = Console.ReadLine();
         if (livello == "Facile" || livello == "1")
         {
             scelta = true;
+            n_tentativi = 3;
+            Console.WriteLine("\nGuadagnerai 5 punti se indovinerai la parola.\nTentativi disponibili: " + n_tentativi);
             return "parole_facili.txt";
         }
         else if (livello == "Medio" || livello == "2")
         {
             scelta = true;
+            n_tentativi = 5;
+            Console.WriteLine("\nGuadagnerai 10 punti se indovinerai la parola.\nTentativi disponibili: " + n_tentativi);
             return "parole_medie.txt";
         }
         else if (livello == "Difficile" || livello == "3")
         {
             scelta = true;
+            n_tentativi = 7;
+            Console.WriteLine("\nGuadagnerai 20 punti se indovinerai la parola.\nHai tre indizi a disposizione.\nTentativi disponibili: " + n_tentativi);
             return "parole_difficili.txt";
         }
         else
         {
-            Console.WriteLine("Livello di difficoltà non valido. Per favore reinseriscilo.");
+            Console.WriteLine("\nLivello di difficoltà non valido. Per favore reinseriscilo.\n");
         }
     }
     return "Errore";
 }
-void scelta_tema(string[] parole, string[] range)
+void scelta_tema(string[] parole, string[] range, string[] range_ind, string[] indizi_diff)
 {
     int inizio = 0, fine = 0;
     bool scelta = false;
     while (scelta == false)
     {
-        Console.WriteLine("Scegli il tema:\n1. Paesi\n2. Calciatori\n3. Mestieri\n4. Brand");
+        Console.WriteLine("\nScegli il tema:\n1. Paesi\n2. Calciatori\n3. Mestieri\n4. Brand");
         string tema = Console.ReadLine();
         if (tema == "Paesi" || tema == "1")
         {
@@ -62,66 +68,259 @@ void scelta_tema(string[] parole, string[] range)
         }
         else
         {
-            Console.WriteLine("Tema non valido. Per favore reinseriscilo.");
+            Console.WriteLine("\nTema non valido. Per favore reinseriscilo.\n ");
         }
-        int pos = 0;
+        int pos = 0, pos1 = 0;
         for (int i = inizio; i <= fine; i++)
         {
             range[pos++] = parole[i];
+            range_ind[pos1++] = indizi_diff[i]; 
         }
     }
 }
-string sostituzione(string segreta)
+char[] sostituzione(string segreta)
 {
-    string sostituita = "";
+    char[] sostituita = new char[segreta.Length];
     for(int i = 0; i < segreta.Length; i++)
     {
-        sostituita += "_";
+        sostituita[i] = '_';
     }
     return sostituita;
 }
-Console.WriteLine("Benvenuto nel gioco dell'impiccato.\nIl gioco consiste nell'indovinare una parola, dovrai scegliere la difficoltà e se vorrai il tema, inserendo un carattere o provando direttamente la parola.\nBuona fortuna!");
-bool chiudi = false;
-while (chiudi == false)
+char[] stringa_array(string segreta)
 {
-    string file = scelta_livello();
-    string[] parole = File.ReadAllLines(file);
-    string[] range = new string[10];
-    scelta_tema(parole, range);
-    int indice_casuale = r.Next(range.Length);
-    string segreta = range[indice_casuale - 1];
-    Console.WriteLine(segreta);
-    string sostituita = sostituzione(segreta);
-    Console.WriteLine("\nCaricamento della parola in corso....\n");
-    Console.WriteLine(sostituita);
-    int tent = 0;
-    while(sostituita != segreta)
+    char[] parola_segreta = new char[segreta.Length];
+    for (int i = 0; i < segreta.Length; i++)
     {
-        if(tent < 2)
+        parola_segreta[i] = segreta[i];
+    }
+    return parola_segreta;
+}
+void tentativi_finiti(int n_tentativi, ref int lettere_indovinate, string segreta, ref string non_indovinate)
+{
+    if(n_tentativi <= 0)
+    {
+        Console.WriteLine("\nHai esaurito i tentativi. Parola non indovinata.");
+        non_indovinate += segreta + "  ";
+        lettere_indovinate = segreta.Length;
+    }
+}
+int punteggio(string livello)
+{
+    int punti = 0;
+    if (livello == "Facile" || livello == "1")
+    {
+        punti = 5;
+    }
+    else if (livello == "Medio" || livello == "2")
+    {
+        punti = 10;
+    }
+    else if (livello == "Difficile" || livello == "3")
+    {
+        punti = 20;
+    }
+    return punti;
+}
+void jolly(ref char[] sostituta, string segreta, ref int n_jolly)
+{
+    if (n_jolly > 0)
+    {
+        if (sostituta[0] == '_' || sostituta[sostituta.Length - 1] == '_')
         {
-            Console.WriteLine("1. Inserisci lettera\n2. Chiudi");
-            string comando = Console.ReadLine();
-            if (comando == "2" || comando == "Chiudi")
+            Console.WriteLine("\n\nVuoi utilizzare il jolly? (si o no)");
+            string ut_jolly = Console.ReadLine();
+            if (ut_jolly == "si")
             {
-                sostituita = segreta;
-                chiudi = true;
+                sostituta[0] = segreta[0];
+                sostituta[sostituta.Length - 1] = segreta[sostituta.Length - 1];
+                Console.WriteLine("\nJolly utilizzato. Prima e ultima lettera indovinate.");
+                n_jolly--;
             }
-            else if(comando == "1" || comando == "Inserisci lettera")
+            for (int i = 0; i < sostituta.Length; i++)
             {
-                Console.WriteLine("\nInserisci lettera: ");
-                string lettera = Console.ReadLine();
-                if (segreta.Contains(lettera))
-                {
-                    Console.WriteLine("\nBravo, lettera indovinata!\n");
-                    for(int i = 0; i < segreta.Length; i++)
-                    {
-                        if (segreta[i] == lettera)
-                        {
-                            sostituita[i] = lettera;
-                        }
-                    }
-                }
+                Console.Write(sostituta[i]);
             }
         }
     }
 }
+string ins_indizi(string livello)
+{
+    if (livello == "Difficile" || livello == "3")
+    {
+        return "indizi_difficili.txt";
+    }
+    return "";
+}
+void utilizzo_indizi(ref int n_indizi, string livello, char[] sostituta, string indizio)
+{
+    if (n_indizi > 0)
+    {
+        if (livello == "3" || livello == "Difficile")
+        {
+            Console.WriteLine("\n\nVuoi l'indizio? (si o no)");
+            string utilizzo = Console.ReadLine();
+            if (utilizzo == "si")
+            {
+                n_indizi--;
+                Console.WriteLine(indizio);
+            }
+        }
+        for (int i = 0; i < sostituta.Length; i++)
+        {
+            Console.Write(sostituta[i]);
+        }
+    }
+}
+void indovina_parola(ref char[] sostituta, char[] parola_segreta, ref bool chiudi, string segreta, ref int n_tentativi, ref string p_indovinate, ref string non_indovinate, string livello, ref int punteggio_tot, ref int n_jolly, ref int n_indizi, string[] indizi_diff, string indizio)
+{
+    int lettere_indovinate = 0, punti;
+    string l_provate = "", p_provate = "";
+    while (lettere_indovinate != segreta.Length)
+    {
+        punti = 0;
+        for(int i = 0; i < sostituta.Length; i++)
+        {
+            Console.Write(sostituta[i]);
+        }
+        jolly(ref sostituta, segreta, ref n_jolly);
+        utilizzo_indizi(ref n_indizi, livello, sostituta, indizio);
+        Console.WriteLine("\n\n1. Inserisci lettera\n2. Prova parola\n3. Chiudi");
+        string comando = Console.ReadLine();
+        Console.WriteLine("\nTentativi rimasti: " + n_tentativi);
+        if (comando == "3" || comando == "Chiudi")
+        {
+            lettere_indovinate = segreta.Length;
+            chiudi = true;
+        }
+        else if (comando == "1" || comando == "Inserisci lettera")
+        {
+            char[] lettere_provate = stringa_array(l_provate);
+            Console.WriteLine("\nLettere provate: ");
+            for (int i = 0; i < lettere_provate.Length; i++)
+            {
+                Console.Write(lettere_provate[i] + "   ");
+            }
+            Console.WriteLine("\nInserisci lettera: ");
+            char lettera = Console.ReadLine()[0];
+            if (l_provate.Contains(lettera))
+            {
+                Console.WriteLine("\nLettera già inserita.\n");
+            }
+            else
+            {
+                l_provate += lettera;
+                if (segreta.Contains(lettera))
+                {                    
+                    Console.WriteLine("\nBravo, lettera indovinata!\n");
+                    for (int i = 0; i < parola_segreta.Length; i++)
+                    {
+                        if (parola_segreta[i] == lettera && sostituta[i] != lettera)
+                        {
+                            sostituta[i] = lettera;
+                            lettere_indovinate++;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nLettera errata. Tentativo perso.");
+                    n_tentativi--;
+                }
+                if(lettere_indovinate == segreta.Length)
+                {
+                    Console.WriteLine("\nComplimenti! Parola indovinata.");
+                    p_indovinate += segreta + " ";
+                    punti = punteggio(livello);
+                }
+                tentativi_finiti(n_tentativi, ref lettere_indovinate, segreta, ref non_indovinate);
+            }
+        }
+        else if (comando == "2" || comando == "Prova parola")
+        {
+            string[] parole_provate = p_provate.Split(' ');
+            Console.WriteLine("\nParole provate: ");
+            for (int i = 0; i < parole_provate.Length; i++)
+            {
+                Console.Write(parole_provate[i] + " ");
+            }
+            Console.WriteLine("\nInserisci parola:");
+            string parola_utente = Console.ReadLine();
+            if (p_provate.Contains(parola_utente))
+            {
+                Console.WriteLine("\nParola già inserita.\n");
+            }
+            else
+            {
+                p_provate += parola_utente + " ";
+                if (parola_utente.ToLower() == segreta.ToLower())
+                {
+                    Console.WriteLine("\nComplimenti! Parola indovinata.");
+                    p_indovinate += parola_utente + " ";
+                    lettere_indovinate = segreta.Length;
+                    punti = punteggio(livello);
+                }
+                else
+                {
+                    Console.WriteLine("\nParola errata. Ritenta quando sarai più sicuro.");
+                    n_tentativi--;
+                }
+                tentativi_finiti(n_tentativi, ref lettere_indovinate, segreta, ref non_indovinate);
+            }
+        }
+        punteggio_tot += punti;
+    }
+}
+Console.WriteLine("Benvenuto nel gioco dell'impiccato.\nIl gioco consiste nell'indovinare una parola, scegliendo la difficoltà ed il tema ed inserendo un carattere o la parola.\nHai a disposizione tre jolly in totale, permettono di rivelare la prima e l'ultima lettera.\nBuona fortuna!");
+bool chiudi = false, utilizzata;
+int indice_casuale = -1, n_tentativi = 0, punteggio_tot = 0, n_jolly = 3, n_indizi = 3;
+string livello = "", file, segreta = "", p_indovinate = "", non_indovinate = "", indizi, indizio = "";
+while (chiudi == false)
+{
+    file = scelta_livello(ref n_tentativi, ref livello);
+    string[] parole = File.ReadAllLines(file);
+    string[] range = new string[10];
+    indizi = ins_indizi(livello);
+    string[] indizi_diff = File.ReadAllLines(indizi);
+    string[] range_ind = new string[10];
+    scelta_tema(parole, range, range_ind, indizi_diff);
+    utilizzata = true;
+    while(utilizzata == true)
+    {
+        indice_casuale = r.Next(range.Length - 1);
+        segreta = range[indice_casuale];
+        indizio = range_ind[indice_casuale];
+        if (p_indovinate.Contains(segreta) == false)
+        {
+            utilizzata = false;
+        }
+    }
+    char[] parola_segreta = stringa_array(segreta);
+    char[] sostituta = sostituzione(segreta);
+    Console.WriteLine("\nCaricamento della parola in corso....\n");
+    indovina_parola(ref sostituta, parola_segreta, ref chiudi, segreta, ref n_tentativi, ref p_indovinate, ref non_indovinate, livello, ref punteggio_tot, ref n_jolly, ref n_indizi, indizi_diff, indizio);
+}
+string[] parole_indovinate = p_indovinate.Split(' ');
+string[] parole_non_indovinate = non_indovinate.Split(' ');
+for(int i = 0; i <  parole_non_indovinate.Length; i++)
+{
+    for(int j = 0; j < parole_indovinate.Length; j++)
+    {
+        if (parole_non_indovinate[i] == parole_indovinate[j])
+        {
+            parole_non_indovinate[i] = "";
+        }
+    }
+}
+Console.WriteLine("\nParole indovinate: ");
+for(int i = 0; i < parole_indovinate.Length;  i++)
+{
+    Console.Write(parole_indovinate[i] + "  ");
+}
+Console.WriteLine("\nParole non indovinate: ");
+for (int i = 0; i < parole_non_indovinate.Length; i++)
+{
+    Console.Write(parole_non_indovinate[i] + "  ");
+}
+Console.WriteLine("\n\nPunteggio totale: " + punteggio_tot);
+Console.WriteLine("\n\nGioco terminato.");
--- Gioco_Impiccato/obj/Debug/net7.0/Gioco_Impiccato.AssemblyInfo.cs
+++ Gioco_Impiccato/obj/Debug/net7.0/Gioco_Impiccato.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gioco_Impiccato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b0789b3ad1c61a9c0c5f3cef45dd172e2a865f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gioco_Impiccato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gioco_Impiccato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
